6271d6ef58130b2cbb337ffc90333ffcc6a243b6
Michael Blatchford <mblatchford@mac.com>
2019-01-27 10:30:17 -0500

    production build

---
+++ build/precache-manifest.26c3645e8f4ba75611dae10b575564d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dce96c0f2294da6cdc13",
+    "url": "/static/js/main.dce96c0f.chunk.js"
+  },
+  {
+    "revision": "85567b4c6412ea1d1036",
+    "url": "/static/js/1.85567b4c.chunk.js"
+  },
+  {
+    "revision": "dce96c0f2294da6cdc13",
+    "url": "/static/css/main.2b902fb0.chunk.css"
+  },
+  {
+    "revision": "ebb7364c21f4dfe8670653d5730167f9",
+    "url": "/index.html"
+  }
+];
